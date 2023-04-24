81f18e53b1ae01746bad70a52992d927bb020a84
Fabian <contacto.fabianmartinezcaro@gmail.com>
2023-04-24 14:01:50 -0300

    nuevo

--- js/funciones.js
+++ js/funciones.js
@@ -1,25 +1,25 @@
 import UI from "./classes/UI.js";
 import { DB } from "./db.js";
-import { formulario, nombre, email, telefono, empresa } from "./selectores.js";
+import { formulario, inputNombre, inputEmail, inputTelefono, inputEmpresa } from "./selectores.js";
 
 const ui = new UI();
 
 export function validarCliente (evento) {
     evento.preventDefault();
 
-    if(nombre.value === '' || email.value === '' || telefono.value === '' || empresa.value === ''){
+     const nombre = document.querySelector('#nombre').value;
+     const email = document.querySelector('#email').value;
+     const telefono = document.querySelector('#telefono').value;
+     const empresa = document.querySelector('#empresa').value;
+
+    if(nombre === '' || email === '' || telefono === '' || empresa === ''){
         ui.mostrarAlerta(formulario, 'Todos los campos son obligatorios.', 'error');
         return;
     }
 
-    const cliente = {
-        nombre: nombre.value, 
-        email: email.value, 
-        telefono: telefono.value, 
-        empresa: empresa.value, 
-        id: Date.now()
-    };
+    const cliente = {nombre, email, telefono, empresa, id: Date.now()};
 
+    console.log(cliente)
     agregarCliente(cliente);
 
 }
@@ -27,7 +27,6 @@ export function validarCliente (evento) {
 function agregarCliente(cliente){
 
     const transaction = DB.transaction(['clientes'], 'readwrite');
-    console.log(DB)
     const objectStore = transaction.objectStore('clientes');
 
     objectStore.add(cliente);
@@ -46,44 +45,18 @@ export function editarCliente(){
 
     const parametroURL = new URLSearchParams(window.location.search);
     const idCliente = parametroURL.get('id');
-    console.log(idCliente)
-
-    const abrirConexion = window.indexedDB.open('dbClientes', 1);
-
-    abrirConexion.onerror = function () {
-        console.log('Hubo un error');
+    if(idCliente){
+        setTimeout(() => {
+            obtenerRegistro(idCliente)
+        }, 100);
     }
 
-    abrirConexion.onsuccess = function () {
-        console.log('Se ha conectado correctamente!')
-        let DB = abrirConexion.result;
-
-        const objectStore = DB.transaction('clientes', 'readwrite').objectStore('clientes');
-
-
-        objectStore.openCursor().onsuccess = function (evento) {
-            const cliente = evento.target.result;
-            
-            console.log('ID INDEXED:', cliente.value.id)
-            console.log('ID CLIENTE:', parseInt(idCliente))
-            if(cliente.value.id === parseInt(idCliente)){
-                const nombre = document.querySelector('#nombre').value;
-                const email = document.querySelector('#email').value;
-                const telefono = document.querySelector('#telefono').value;
-                const empresa = document.querySelector('#empresa').value;
-
-                cliente.update({nombre, email, telefono, empresa, id: cliente.value.id});
-            }   
-
-        }
-
-    }
-    
 }
 
 export function eliminarCliente(id){
     console.log('Eliminando...', id)
 }
+
 
 export function obtenerClientes(){
     const abrirConexion = window.indexedDB.open('dbClientes', 1);
@@ -102,9 +75,9 @@ export function obtenerClientes(){
             const cliente = evento.target.result;
             
             if(cliente){
+                
                 ui.mostrarCliente(cliente.value);
                 cliente.continue();
-                return;
             }
 
         }
@@ -114,6 +87,31 @@ export function obtenerClientes(){
 }
 
 function obtenerRegistro(id){
-    console.log(id)
+
+    const transaction = DB.transaction(['clientes'], 'readwrite');
+    const objectStore = transaction.objectStore('clientes');
+
+    const cliente = objectStore.openCursor();
+    cliente.onsuccess = function (evento) {
+        const cursor = evento.target.result;
+        if(cursor){
+            console.log(cursor.value)
+            if(cursor.value.id === Number(id)){
+                llenarFormulario(cursor.value);
+            }
+            cursor.continue();
+        }
+    }
+
+}
+
+function llenarFormulario(datos){
+    const {nombre, email, telefono, empresa} = datos;
+
+    inputNombre.value = nombre;
+    inputEmail.value = email;
+    inputTelefono.value = telefono;
+    inputEmpresa.value = empresa;
+
 }
 
